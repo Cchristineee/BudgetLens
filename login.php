42aa76206a84a2e9ec8f9d99d92857965884c17c
Ariana Brown <arii@Arianas-MacBook-Air.local>
2026-04-13 14:41:26 -0400

    login works with database and connected to dashboard

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+session_start(); 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include "connect.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $stmt = $conn->prepare("SELECT * FROM user_data WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+    
+    
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['userID'];
+        $_SESSION['username'] = $user['username'];
+        header('location:home.html');
+        exit();
+    } else {
+        echo "Invalid username or password.";
+    }
+
+}
+?>
