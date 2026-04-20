64f1951825b776473e4f4bd5d857052f02d2ff77
Christine Grimadeau <cgrimad1@yahoo.com>
2026-04-20 10:36:47 -0400

    Added spending status cards

--- MyBudget.php
+++ MyBudget.php
@@ -20,7 +20,35 @@ session_start()
             <a href="#" class="create-budget-btn">+ Create Budget</a>
         </div>
 
-        <!-- Spending Status card -->
-     
+        <!-- Spending Status cards ❤ -->
+        <section class="budgeting-cards">
+            <div class="budgeting-card">
+                <h3 class ="card-title food">🍱 Food</h3>
+                <p class="card-text">$200.50 of $300.00 spent</p>
+                <div class="progress-bar">
+                    <div class="progress-fill" style="width: 67%;"></div>
+            </div>
+
+            <div class="budgeting-card">
+                <h3 class ="card-title health">❤️ Health</h3>
+                <p class="card-text">$150.00 of $100.00 spent</p>
+                <div class="progress-bar">
+                    <div class="progress-fill danger" style="width: 100%;"></div>
+            </div>
+
+            <div class="budgeting-card">
+                <h3 class ="card-title household">🏠 Household</h3>
+                <p class="card-text">$300.00 of $500.00 spent</p>
+                <div class="progress-bar">
+                    <div class="progress-fill" style="width: 60%;"></div>
+            </div>  
+
+            <div class="budgeting-card">
+                <h3 class ="card-title clothing">🛍️ Clothing</h3>
+                <p class="card-text">$250.01 of $1,100.00 spent</p>
+                <div class="progress-bar">
+                    <div class="progress-fill" style="width: 23%;"></div>
+            </div>     
+</section>
 </body>
 </html>
