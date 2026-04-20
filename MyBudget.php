a68a6854fba656331a79ae9593e071cbf676f69f
Christine Grimadeau <cgrimad1@yahoo.com>
2026-04-19 21:23:01 -0400

    Added My Budget section

---
+++ MyBudget.php
@@ -0,0 +1,26 @@
+<?php
+session_start()
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BudgetLens - My Budget</title>
+     <link rel="stylesheet" href="MyBudget.css">
+</head>
+<body>
+    <div class="dashboard-layout">
+    
+    <?php include 'sidebar.php'; ?>
+
+     <main class="main-content">
+        <div class="page-header">
+            <h1>My Budget</h1>
+            <a href="#" class="create-budget-btn">+ Create Budget</a>
+        </div>
+
+        <!-- Spending Status card -->
+     
+</body>
+</html>
