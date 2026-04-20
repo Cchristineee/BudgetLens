76a99b3a777500130b5314fc4b46578abd3dbeac
Christine Grimadeau <cgrimad1@yahoo.com>
2026-04-20 13:48:26 -0400

    Added list details

---
+++ List_details.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BudgetLens - List details</title>
+    <link rel="stylesheet" href="List_details.css">
+</head>
+<body>
+    <div class="dashboard-layout">
+
+    <!-- Reusing sidebar throughout ★ -->
+    <?php include 'sidebar.php';?>
+
+    <main class="main-content">
+        <div class="page-header">
+            <h1>My List</h1>
+            <a href="#" class="share-btn">Share</a>
+        </div>
+</body>
+</html>
