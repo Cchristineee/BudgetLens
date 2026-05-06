5b399767a55a52df4fc1241f8a3d1a3bb97cc90b
Christine Grimadeau <cgrimad1@yahoo.com>
2026-05-06 02:02:23 -0400

    created admin categories and styles

--- admin_categories.php
+++ admin_categories.php
@@ -17,4 +17,41 @@ session_start();
     <?php include 'admin_sidebar.php'; ?>
 
     <!-- Main content ★ -->
+    <main class="admin-categories">
+        <h1>All Categories</h1>
+    
+    <!-- Admin could add a new category to existing shopping list ★ -->
+    <section class="card">
+        <h2>Add New Category</h2>
+
+        <form class="category-form">
+            <input type="text" placeholder="Enter a category name...." name="categoryName"
+            />
+
+            <button type="submit">
+                <span>+</span>
+                Add
+            </button>
+        </form>
+    </section>
+
+    <!-- Admin could view all categories ★ -->
+    <!-- Hardcoded......★ -->
+    <section class="card">
+        <h2>All Categories</h2>
+
+        <div class="category-item">Food</div>
+        <div class="category-item">Clothing</div>
+        <div class="category-item">Cleaning</div>
+        <div class="category-item">Health</div>
+        <div class="category-item">Entertainment</div>
+      </div>
+    </section>
+    </main>
+
+    </div>
+</body>
+</html>
+
+    
     
